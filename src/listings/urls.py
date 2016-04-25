2b242524c9e7cd2c818d3268e48b12c726bb5290
carstenpleiser <carsten.pleiser@gmx.de>
2016-04-25 16:23:14 +0100

    Added CBV for DetailView and ListView, implemented templates

--- src/listings/urls.py
+++ src/listings/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
+from listings.views import (ListingListView, ShowListing)
 
 urlpatterns = [
-    url(r'^$', views.index, name='listings'),
+    url(r'^$', views.ListingListView.as_view(), name='listings'),
+    url(r'^(?P<slug>[\w\-]+)$', views.ShowListing.as_view(),
+        name='listing_detail'),
 ]
--- src/listings/views.py
+++ src/listings/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from .models import Listing
+from django.http import Http404
+from django.views import generic
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from listings.models import Listing
+from braces.views import LoginRequiredMixin
+from django.contrib import messages
+from listings.forms import ListingForm
 
 # Create your views here.
-def index(request):
-    listings = Listing.objects.prefetch_related('user').all()
-    return render(request, "index.html", {
-        'listings': listings,
-    })
+#def index(request):
+#    listings = Listing.objects.prefetch_related('user').all()
+#    return render(request, "index.html", {
+#        'listings': listings,
+#    })
+
+class ListingListView(ListView):
+    model = Listing
+    template_name = "index.html"
+    queryset = Listing.objects.prefetch_related('user').distinct()
+
+class ShowListing(DetailView):
+    model = Listing
+    template_name = "listings/show_listing.html"
+
