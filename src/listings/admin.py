c43401abfaba638cac524590c6e80bcb83909b51
carstenpleiser <carsten.pleiser@gmx.de>
2016-04-27 14:56:05 +0100

    Added additional admin actions to publish and unpublish listings

--- src/listings/admin.py
+++ src/listings/admin.py
@@ -5,6 +5,18 @@ from listings.models import Listing
 class ListingAdmin(admin.ModelAdmin):
     model = Listing
     list_display = ('title', 'location', 'slug', 'price', 'user', 'approved')
+    actions = ['publish', 'unpublish']
+
+    def publish(self, request, queryset):
+        queryset.update(approved=True)
+
+    def unpublish(self, request, queryset):
+        queryset.update(approved=False)
+
+
+    publish.short_description = "Publish Listings"
+    unpublish.short_description = "Unpublish Listings"
+
 
 # Register your models here.
 admin.site.register(Listing, ListingAdmin)
